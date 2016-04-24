687594d22708ad563333a678c5c3f8d4b0f96c07
Randy Brunecz <rkbrunecz@gmail.com>
2016-04-24 01:08:44 -0400

    Added a general purpose custom font creator. Streamlines the process.

---
+++ app/src/main/java/com/example/randy/to_be_determined/CustomFont.java
@@ -0,0 +1,30 @@
+package com.example.randy.to_be_determined;
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.CheckBox;
+
+/**
+ * Created by Randy on 4/23/2016.
+ */
+public class CustomFont {
+    public static void setCustomFont(String fontName, TextView txt, AssetManager am)
+    {
+        Typeface font = Typeface.createFromAsset(am, "fonts/" + fontName);
+        txt.setTypeface(font);
+    }
+
+    public static void setCustomFont(String fontName, Button btn, AssetManager am)
+    {
+        Typeface font = Typeface.createFromAsset(am, "fonts/" + fontName);
+        btn.setTypeface(font);
+    }
+
+    public static void setCustomFont(String fontName, CheckBox box, AssetManager am)
+    {
+        Typeface font = Typeface.createFromAsset(am, "fonts/" + fontName);
+        box.setTypeface(font);
+    }
+}
