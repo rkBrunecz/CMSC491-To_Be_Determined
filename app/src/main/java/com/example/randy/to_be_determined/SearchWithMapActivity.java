fffac588e4cfdb7ceae6614129dd8961ba57de88
Randy Brunecz <rkbrunecz@gmail.com>
2016-05-03 00:55:04 -0400

    Added support with the post database

--- app/src/main/java/com/example/randy/to_be_determined/SearchWithMapActivity.java
+++ app/src/main/java/com/example/randy/to_be_determined/SearchWithMapActivity.java
@@ -1,5 +1,8 @@
 package com.example.randy.to_be_determined;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -13,25 +16,76 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+/**
+ * This class displays a map activity that updates dynamically with markers to show which buildings
+ * on campus have spots available for the user to reserve.
+ *
+ * Resources:
+ * http://www.mopri.de/2010/timertask-bad-do-it-the-android-way-use-a-handler/
+ * https://developers.google.com/maps/documentation/android-api/marker#customize_a_marker
+ * http://stackoverflow.com/questions/15925319/how-to-disable-android-map-marker-click-auto-center
+ * https://developers.google.com/maps/documentation/android-api/infowindows#custom_info_windows
+ */
 public class SearchWithMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
+
+    /* PRIVATE CONSTANTS */
+    private final int DELAY = 1000;
 
     /* PRIVATE VARIABLES */
     private GoogleMap mMap;
     private MapMarker campusBuildings[] = new MapMarker[15];
+    private Handler handler = new Handler();
+    private DatabaseHelper dbhelper;
+    private SQLiteDatabase db;
 
     private class MapMarker{
         /* PRIVATE VARIABLES */
-        private LatLng pos;
         private String name;
+        private int numSpotsAvailable = 0;
+        private MarkerOptions markerOps = new MarkerOptions();
+        private Marker marker;
 
-        public MapMarker(String name, LatLng pos)
-        {
+        public MapMarker(String name, LatLng pos) {
             this.name = name;
-            this.pos = pos;
+
+            markerOps.position(pos);
+            markerOps.title(name);
+            markerOps.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+            markerOps.snippet("Number of Spots: " + numSpotsAvailable);
+            markerOps.visible(false);
         }
 
-        public String getName(){ return name;}
-        public LatLng getPos() {return pos;}
+        public void updateNumSpots(int numSpotsAvailable)
+        {
+            this.numSpotsAvailable = numSpotsAvailable;
+            marker.setSnippet("Number of Spots: " + this.numSpotsAvailable);
+
+            if(this.numSpotsAvailable > 0)
+                marker.setVisible(true);
+            else
+                marker.setVisible(false);
+        }
+    }
+
+    private class UpdateMapMarkers implements Runnable{
+        public UpdateMapMarkers()
+        {
+
+        }
+
+        @Override
+        public void run() {
+            if (mMap != null)
+            {
+                for (int i = 0; i < 15; i++) {
+                    Cursor locs = fetchLocations(campusBuildings[i].name, ((SpotSwap)getApplication()).getUserName());
+                    campusBuildings[i].updateNumSpots(locs.getCount());
+                    locs.close();
+                }
+            }
+
+            handler.postDelayed(this, DELAY);
+        }
     }
 
     @Override
@@ -44,7 +98,7 @@ public class SearchWithMapActivity extends FragmentActivity implements OnMapRead
         mapFragment.getMapAsync(this);
 
         /* Intialize the campus buildings hashtable */
-        campusBuildings[0] = new MapMarker("Performing Arts & Humanities", new LatLng(39.2549, -76.7147));
+        campusBuildings[0] = new MapMarker("Performing Arts and Humanities", new LatLng(39.2549, -76.7147));
         campusBuildings[1] = new MapMarker("Information Technology / Engineering", new LatLng(39.2537, - 76.7142));
         campusBuildings[2] = new MapMarker("Engineering", new LatLng(39.2544, -76.7139));
         campusBuildings[3] = new MapMarker("Fine Arts", new LatLng(39.2550, -76.7135));
@@ -52,13 +106,30 @@ public class SearchWithMapActivity extends FragmentActivity implements OnMapRead
         campusBuildings[5] = new MapMarker("University Center", new LatLng(39.2542, -76.7133));
         campusBuildings[6] = new MapMarker("Meyerhoff", new LatLng(39.2549, -76.7127));
         campusBuildings[7] = new MapMarker("Sondheim Hall", new LatLng(39.2533, -76.7128));
-        campusBuildings[8] = new MapMarker("Math & Psychology", new LatLng(39.2541, -76.7124));
+        campusBuildings[8] = new MapMarker("Math and Psychology", new LatLng(39.2541, -76.7124));
         campusBuildings[9] = new MapMarker("Biological Sciences", new LatLng(39.2547, -76.7122));
         campusBuildings[10] = new MapMarker("The Commons", new LatLng(39.2548, -76.7108));;
         campusBuildings[11] = new MapMarker("Physics", new LatLng(39.2544, -76.7095));
         campusBuildings[12] = new MapMarker("Public Policy", new LatLng(39.2552, -76.7091));
-        campusBuildings[13] = new MapMarker("Albin O. Kuhn Library", new LatLng(39.2565, -76.7114));
+        campusBuildings[13] = new MapMarker("Library", new LatLng(39.2565, -76.7114));
         campusBuildings[14] = new MapMarker("Retriever Activities Center", new LatLng(39.2528, -76.7124));
+
+        dbhelper = new DatabaseHelper(getApplicationContext());
+        db = dbhelper.getWritableDatabase();
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        handler.removeCallbacks(null);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        handler.postDelayed(new UpdateMapMarkers(), DELAY);
     }
 
     /**
@@ -73,6 +144,7 @@ public class SearchWithMapActivity extends FragmentActivity implements OnMapRead
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        mMap.clear();
 
         mMap.getUiSettings().setAllGesturesEnabled(false);
         mMap.getUiSettings().setCompassEnabled(false);
@@ -90,9 +162,7 @@ public class SearchWithMapActivity extends FragmentActivity implements OnMapRead
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(umbcCamera));
 
         for(int i = 0; i < 15; i++)
-            mMap.addMarker(new MarkerOptions().position(campusBuildings[i].getPos())
-                    .title(campusBuildings[i].getName())
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)));
+           campusBuildings[i].marker = mMap.addMarker(campusBuildings[i].markerOps);
     }
 
     @Override
@@ -105,5 +175,10 @@ public class SearchWithMapActivity extends FragmentActivity implements OnMapRead
                 .zoom(16.3f)
                 .build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(umbcCamera));
+    }
+
+    public Cursor fetchLocations(String loc, String username)
+    {
+        return db.query("posts", new String[]{"location"}, "location=? and reserved=? and username!=?", new String[]{loc, "No", username}, null, null, null);
     }
 }
