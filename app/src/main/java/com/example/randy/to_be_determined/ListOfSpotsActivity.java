0df6e91cc3ad8edf4aa5c82b28216c0617f25a26
jlrolfe <jessica.l.rolfe@gmail.com>
2016-05-08 14:00:35 -0400

    Added xml template for new spots, manually creates 10 spots

--- app/src/main/java/com/example/randy/to_be_determined/ListOfSpotsActivity.java
+++ app/src/main/java/com/example/randy/to_be_determined/ListOfSpotsActivity.java
@@ -3,7 +3,10 @@ package com.example.randy.to_be_determined;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class ListOfSpotsActivity extends AppCompatActivity {
 
@@ -12,7 +15,18 @@ public class ListOfSpotsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_spots);
 
-        Button new_spot = new Button(this);
+        LinearLayout layout = (LinearLayout) findViewById(R.id.spots);
+
+        for (int i = 0; i < 10; i++) {
+            View spotLayout = addSpot();
+            layout.addView(spotLayout);
+        }
     }
 
+    public View addSpot() {
+        LinearLayout spots = (LinearLayout) findViewById(R.id.spots);
+        View newSpot = LayoutInflater.from(this).inflate(R.layout.spot_entry, spots, false);
+
+        return newSpot;
+    }
 }
