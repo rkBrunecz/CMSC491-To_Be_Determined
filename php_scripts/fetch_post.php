c6149b6483eb7b7ee4d0fa8027f949d21eb73509
Randy Brunecz <rkbrunecz@gmail.com>
2016-05-09 03:00:25 -0400

    Minor changes

--- php_scripts/fetch_post.php
+++ php_scripts/fetch_post.php
@@ -21,13 +21,12 @@ FROM Posts
 WHERE username!='". $user_name. "' and location='". $location. "' and reservedto=''";
 $result = $conn->query($sql);
 
-echo $result->num_rows. "<br>";
+echo $result->num_rows. "\n";
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
 		echo $row["_id"]. "~". $row["username"]. "~". $row["location"]. "~". $row["floor"]. "~". $row["numseats"]. "~". $row["description"]. "~". $row["windowseat"]. "~".
-		$row["poweroutlet"]. "~". $row["pc"]. "~". $row["whiteboard"]. "~". $row["maccomputers"]. "~". $row["rockingchair"]. "~". $row["silence"]. "~". 
-		$row["image"]. "<br>"; 
+		$row["poweroutlet"]. "~". $row["pc"]. "~". $row["whiteboard"]. "~". $row["maccomputers"]. "~". $row["rockingchair"]. "~". $row["silence"]. "\n"; 
 	}
 } 
 
